1a8db1792742194adef4d0b628446b560f67bd56
stasshur3@yandex.ru <stasshur3@yandex.ru>
2025-10-02 23:49:30 +0300

    HomeWork_18

--- addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -9,12 +9,14 @@ namespace addressbook_tests_autoit
     public class GroupHelper: HelperBase
     {
         public static string GROUPWINTITLE = "Group editor";
+        public static string GROUPWINDELETE = "Delete group";
         public GroupHelper(ApplicationManager manager) : base(manager) { }
 
         public List<GroupData> GetGroupList()
         {
             List<GroupData> list = new List<GroupData>();
             OpenGroupsDialogue();
+            System.Threading.Thread.Sleep(500);
             string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount","#0","");
             for (int i = 0; i < int.Parse(count); i++)
             {
@@ -30,6 +32,18 @@ namespace addressbook_tests_autoit
             return list;
         }
 
+        public void Delete(int index)
+        {
+            OpenGroupsDialogue();
+            aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "Select", "#0|#"+index, "");
+            System.Threading.Thread.Sleep(500);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+            System.Threading.Thread.Sleep(500);
+            aux.ControlClick(GROUPWINDELETE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            System.Threading.Thread.Sleep(500);
+            CloseGroupsDialogue();
+        }
+
         public void Add(GroupData newGroup)
         {
             OpenGroupsDialogue();
@@ -40,7 +54,7 @@ namespace addressbook_tests_autoit
             aux.Send("^a"); // Ctrl+A - выделить все
             aux.Send("{DEL}"); // Удалить
 
-            aux.Send("stas");
+            aux.Send(newGroup.Name);
             Console.WriteLine("Текст 'test' отправлен");
             System.Threading.Thread.Sleep(300);
             aux.Send("{ENTER}");
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/DeleteGroupsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class DeleteGroupsTest : TestBase
+    {
+        [Test]
+
+    public void TestDeleteGrous()
+        {
+            GroupData newGroup = new GroupData()
+            {
+                Name = "forDelete"
+            };
+
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            System.Threading.Thread.Sleep(500);
+            if (oldGroups.Count < 2 )
+            {
+                app.Groups.Add(newGroup);
+                
+            }
+            List<GroupData> groupToOld = app.Groups.GetGroupList();
+
+            System.Threading.Thread.Sleep(500);
+            app.Groups.Delete(0);
+            System.Threading.Thread.Sleep(500);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            //oldGroups.RemoveAt(0);
+            //oldGroups.Sort();
+            //newGroups.Sort();
+
+            Assert.That(groupToOld.Count-1, Is.EqualTo(newGroups.Count));
+        }
+
+    }
+}
