7a09eea41adc7051c3b5243ddac989b7b56ec7b7
stasshur3@yandex.ru <stasshur3@yandex.ru>
2025-10-02 08:18:43 +0300

    MR_18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        private GroupHelper groupHelper;
+        private AutoItX3 aux;
+        public static string WINTITLE = "Free Address Book";
+
+        public ApplicationManager()
+        {
+            aux = new AutoItX3();
+            groupHelper = new GroupHelper(this);
+           
+            aux.Run(@"C:\tools\AppForTesting\AddressBook.exe","",aux.SW_SHOW);
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+        }
+
+
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d510");
+        }
+
+        public AutoItX3 Aux
+        {
+            get { return aux; }
+        }
+
+        public GroupHelper Groups
+        {
+            get { return groupHelper; }
+            
+        }
+    }
+}
